1f5676b007e44a8f018fdc6377306cbdd31e05aa
SamuelAlvys <samuel.alvyys@gmail.com>
2024-05-20 11:43:32 -0300

    adicionando modificaçães no painel tkinter

--- main.py
+++ main.py
@@ -2,9 +2,16 @@
 from tkinter import *
 from tkinter import ttk
 
+#Cores
+cor1 = "#1e1f1e" # preto
+
 janela = Tk()
 janela.title("Calculadora")
 janela.geometry("235x318")
+janela.config(bg=cor1)
+
+frame_tela = Frame(janela, width=235, height=50)
+frame_tela.grid(row=0, column=0)
 
 janela.mainloop()
 
